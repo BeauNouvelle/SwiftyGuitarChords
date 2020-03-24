a1fabf13fbb2b75d9539faaf7aed2d9364b7fa7d
Beau Nouvelle <getswifty@mailfence.com>
2020-03-24 12:21:48 +1100

    Marked properties as public

--- Sources/GuitarChords/GuitarChords.swift
+++ Sources/GuitarChords/GuitarChords.swift
@@ -6,11 +6,11 @@ typealias Suffix = String
 
 public struct GuitarChords {
 
-    static let keys: [Key] = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"]
+    public static let keys: [Key] = ["C", "C#", "D", "Eb", "E", "F", "F#", "G", "Ab", "A", "Bb", "B"]
 
-    static let suffixes: [Suffix] = ["major", "minor", "dim", "dim7", "sus2", "sus4", "7sus4", "7sg", "alt", "aug", "6", "69", "7", "7b5", "aug7", "9", "9b5", "aug9", "7b9", "7#9", "11", "9#11", "13", "maj7", "maj7b5", "maj7#5", "maj9", "maj11", "maj13", "m6", "m69", "m7", "m7b5", "m9", "m11", "mmaj7", "mmaj7b5", "mmaj9", "mmaj11", "add9", "madd9", "/E", "/F", "/F#", "/G", "/G#", "/A", "/Bb", "/B", "/C", "/C#", "m/B", "m/C", "m/C#", "/D", "m/D", "/D#", "m/D#", "m/E", "m/F", "m/F#", "m/G", "m/G#"]
+    public static let suffixes: [Suffix] = ["major", "minor", "dim", "dim7", "sus2", "sus4", "7sus4", "7sg", "alt", "aug", "6", "69", "7", "7b5", "aug7", "9", "9b5", "aug9", "7b9", "7#9", "11", "9#11", "13", "maj7", "maj7b5", "maj7#5", "maj9", "maj11", "maj13", "m6", "m69", "m7", "m7b5", "m9", "m11", "mmaj7", "mmaj7b5", "mmaj9", "mmaj11", "add9", "madd9", "/E", "/F", "/F#", "/G", "/G#", "/A", "/Bb", "/B", "/C", "/C#", "m/B", "m/C", "m/C#", "/D", "m/D", "/D#", "m/D#", "m/E", "m/F", "m/F#", "m/G", "m/G#"]
 
-    static func allChords() -> [Chord]? {
+    public static func allChords() -> [Chord]? {
         guard let data = ChordsData.data else { return nil }
         let allChords = try? JSONDecoder().decode([Chord].self, from: data)
         return allChords
@@ -19,11 +19,11 @@ public struct GuitarChords {
 }
 
 public struct Chord: Codable {
-    let key: Key
-    let suffix: Suffix
-    let positions: [ChordPosition]
+    public let key: Key
+    public let suffix: Suffix
+    public let positions: [ChordPosition]
 
-    func path() -> CGPath {
+    public func path() -> CGPath {
         let path = CGMutablePath()
         path.move(to: .zero)
         path.addLine(to: CGPoint(x: 0, y: 10))
@@ -32,9 +32,9 @@ public struct Chord: Codable {
 }
 
 public struct ChordPosition: Codable {
-    let frets: [Int]
-    let fingers: [Int]
-    let baseFret: Int
-    let barres: [Int]
-    let midi: [Int]
+    public let frets: [Int]
+    public let fingers: [Int]
+    public let baseFret: Int
+    public let barres: [Int]
+    public let midi: [Int]
 }
