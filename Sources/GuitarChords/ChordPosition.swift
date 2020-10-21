1d6f2d9477341e5a75da63029120ceb62623a866
Beau Nouvelle <beau@getswifty.dev>
2020-10-21 17:43:24 +1100

    I think I have the logic this time.

--- Sources/GuitarChords/ChordPosition.swift
+++ Sources/GuitarChords/ChordPosition.swift
@@ -147,7 +147,10 @@ public struct ChordPosition: Codable {
                 let dot = frets[index]
                 if dot >= barre {
                     length += 1
-                } else if dot < barre && length == 0 {
+                } else if dot < barre {
+                    if length > 0 {
+                        break
+                    }
                     startOffset = index + 1
                 }
             }
