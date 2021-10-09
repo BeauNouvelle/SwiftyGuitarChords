7c364d28489c5b4551904bb50e1df3d3827e0cbd
Beau Nouvelle <beau@getswifty.dev>
2021-10-09 19:41:02 +1100

    changed name

--- Package.swift
+++ Package.swift
@@ -4,18 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyGuitarChords",
+    name: "SwiftyChords",
     platforms: [.iOS(.v13), .macOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SwiftyGuitarChords",
-            targets: ["SwiftyGuitarChords"]),
+            name: "SwiftyChords",
+            targets: ["SwiftyChords"]),
     ],
     dependencies: [],
     targets: [
         .target(
-            name: "SwiftyGuitarChords",
+            name: "SwiftyChords",
             dependencies: []
 //            resources: [.process("Resources")]
         ),
