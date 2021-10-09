6e6813fd6824c902f8f06fa7d100a907c5f39806
Beau Nouvelle <beau@getswifty.dev>
2021-10-09 19:26:46 +1100

    updated name

--- Package.swift
+++ Package.swift
@@ -4,23 +4,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyChords",
+    name: "SwiftyGuitarChords",
     platforms: [.iOS(.v13), .macOS(.v11)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
-            name: "SwiftyChords",
-            targets: ["SwiftyChords"]),
+            name: "SwiftyGuitarChords",
+            targets: ["SwiftyGuitarChords"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
+    dependencies: [],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SwiftyChords",
+            name: "SwiftyGuitarChords",
             dependencies: [],
             resources: [.process("Resources")]
         ),
