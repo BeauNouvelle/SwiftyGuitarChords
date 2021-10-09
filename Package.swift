c60d5c7a291e7220072ed77df73300daf727bebf
Beau Nouvelle <beau@getswifty.dev>
2021-10-09 19:31:08 +1100

    removed resources

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .target(
             name: "SwiftyGuitarChords",
-            dependencies: [],
-            resources: [.process("Resources")]
+            dependencies: []
+//            resources: [.process("Resources")]
         ),
     ]
 )
