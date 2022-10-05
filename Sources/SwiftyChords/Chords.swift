3a316582dbfad6ec50b9acbac2e91b0ccf5c51be
Beau Nouvelle <beau@getswifty.dev>
2022-10-06 01:37:26 +1100

    Symbol support for Suffixes (#16)
    * added display text string to suffixes with correct capitalization
    
    * about halfway through creating new symbols
    
    * finished adding all new display texts
    
    * added comments

--- Sources/SwiftyChords/Chords.swift
+++ Sources/SwiftyChords/Chords.swift
@@ -81,6 +81,149 @@ public struct Chords {
         case minorSlashFSharp = "m/F#"
         case minorSlashG = "m/G"
         case minorSlashGSharp = "m/G#"
+        
+        /// A suitible string for displaying to users.
+        /// - `accessible:`  "seven flat five". Useful for text to speech.
+        /// - `short:` Maj, min
+        /// - `symbol:` dim⁷, + For the most common uses of symbols in music notation.
+        /// - `altSymbol:` °, ⁺ Alternative examples of the above `symbol` examples.
+        ///
+        /// Advice is to look through this list and choose what is appropriate for your app. Please submit a PR if you'd like to add or change some of these items if you beleive it could be improved.
+        /// Because of this, do not rely on these values to always remain the same. So don't use them as identifiers, or keys.
+        ///
+        /// For accessibility strings the "th" is dropped from numbers. While not completely accurate, it rolls better.
+        ///
+        /// Symbols use superscript where appropriate (and possible). Be aware that not all fonts will support this. Use `short` instead if you're unsure.
+        ///
+        /// Some items may also be identical across types. Only in rare cases will an alt symbol be provided e.g. (`dim⁷`,`°`)
+        var display: (accessible: String, short: String, symbolized: String, altSymbol: String) {
+            switch self {
+            case .major:
+                return (" major", "Maj", "M", "M")
+            case .minor:
+                return (" minor", "min", "m", "m")
+            case .dim:
+                return (" diminished", "dim", "dim", "dim")
+            case .dimSeven:
+                return (" dim seven", "dim7", "dim⁷", "°")
+            case .susTwo:
+                return (" suss two", "sus2", "sus²", "sus²")
+            case .susFour:
+                return (" suss four", "sus4", "sus⁴", "sus⁴")
+            case .sevenSusFour:
+                return (" seven sus four", "7sus4", "⁷sus⁴", "⁷sus⁴")
+            case .altered:
+                return (" alt", "alt", "alt", "alt")
+            case .aug:
+                return (" augmented", "aug", "aug", "⁺")
+            case .six:
+                return (" six", "6", "⁶", "⁶")
+            case .sixNine:
+                return (" six slash nine", "6/9", "⁶ᐟ⁹", "⁶ᐟ⁹")
+            case .seven:
+                return (" seven", "7", "⁷", "⁷")
+            case .sevenFlatFive:
+                return (" seven flat five", "7b5", "⁷♭⁵", "⁷♭⁵")
+            case .augSeven:
+                return (" org seven", "aug7", "aug⁷", "⁺⁷")
+            case .nine:
+                return (" nine", "9", "⁹", "⁹")
+            case .nineFlatFive:
+                return (" nine flat five", "9b5", "⁹♭⁵", "⁹♭⁵")
+            case .augNine:
+                return (" org nine", "aug9", "aug⁹", "⁺⁹")
+            case .sevenFlatNine:
+                return (" seven flat nine", "7b9", "⁷♭⁹", "⁷♭⁹")
+            case .sevenSharpNince:
+                return (" seven sharp nine", "7#9", "⁷♯⁹", "⁷♯⁹")
+            case .eleven:
+                return (" eleven", "11", "¹¹", "¹¹")
+            case .nineSharpEleven:
+                return (" nine sharp eleven", "9#11", "⁹♯¹¹", "⁹♯¹¹")
+            case .thirteen:
+                return (" thirteen", "13", "¹³", "¹³")
+            case .majorSeven:
+                return (" major seven", "Maj7", "Maj⁷", "M⁷")
+            case .majorSevenFlatFive:
+                return (" major seven flat five", "Maj7b5", "Maj⁷♭⁵", "M⁷♭⁵")
+            case .majorSevenSharpFive:
+                return (" major seven sharp five", "Maj7#5", "Maj⁷♯⁵", "M⁷♯⁵")
+            case .majorNine:
+                return (" major nine", "Maj9", "Maj⁹", "M⁹")
+            case .majorEleven:
+                return (" major eleven", "Maj11", "Maj¹¹", "M¹¹")
+            case .majorThirteen:
+                return (" major thirteen", "Maj13", "Maj¹³", "M¹³")
+            case .minorSix:
+                return (" minor six", "m6", "m⁶", "m⁶")
+            case .minorSixNine:
+                return (" minor six slash nine", "m6/9", "m⁶ᐟ⁹", "m⁶ᐟ⁹")
+            case .minorSeven:
+                return (" minor seven", "m7", "m⁷", "m⁷")
+            case .minorSevenFlatFive:
+                return (" minor seven flat five", "m7b5", "m⁷♭⁵", "ø⁷")
+            case .minorNine:
+                return (" minor nine", "m9", "m⁹", "m⁹")
+            case .minorEleven:
+                return (" minor eleven", "m11", "m¹¹", "m¹¹")
+            case .minorMajorSeven:
+                return (" minor major seven", "mMaj7", "mMaj⁷", "mᴹ⁷")
+            case .minorMajorSeventFlatFive:
+                return (" minor major seven flat five", "mMaj7b5", "mMaj⁷♭⁵", "mᴹ⁷♭⁵")
+            case .minorMajorNine:
+                return (" minor major nine", "mMaj9", "mMaj⁹", "mᴹ⁹")
+            case .minorMajorEleven:
+                return (" minor major eleven", "mMaj11", "mMaj¹¹", "mᴹ¹¹")
+            case .addNine:
+                return (" add nine", "add9", "add⁹", "ᵃᵈᵈ⁹")
+            case .minorAddNine:
+                return (" minor add nine", "madd9", "madd⁹", "mᵃᵈᵈ⁹")
+            case .slashE:
+                return (" slash E", "/E", "/E", "/E")
+            case .slashF:
+                return (" slash F", "/F", "/F", "/F")
+            case .slashFSharp:
+                return (" slash F sharp", "/F#", "/F♯", "/F♯")
+            case .slashG:
+                return (" slash G", "/G", "/G", "/G")
+            case .slashGSharp:
+                return (" slash G sharp", "/G#", "/G♯", "/G♯")
+            case .slashA:
+                return (" slash A", "/A", "/A", "/A")
+            case .slashBFlat:
+                return (" slash B flat", "/Bb", "/B♭", "/B♭")
+            case .slashB:
+                return (" slash B", "/B", "/B", "/B")
+            case .slashC:
+                return (" slash C", "/C", "/C", "/C")
+            case .slashCSharp:
+                return (" slash C sharp", "/C#" , "/C♯", "/C♯")
+            case .minorSlashB:
+                return (" minor slash B", "m/B", "m/B", "m/B")
+            case .minorSlashC:
+                return (" minor slash C", "m/C", "m/C", "m/C")
+            case .minorSlashCSharp:
+                return (" minor slash C sharp", "m/C#", "m/C♯", "m/C♯")
+            case .slashD:
+                return (" slash D", "/D", "/D", "/D")
+            case .minorSlashD:
+                return (" minor slash D", "m/D", "m/D", "m/D")
+            case .slashDSharp:
+                return (" slash D sharp", "/D#", "/D♯", "/D♯")
+            case .minorSlashDSharp:
+                return (" minor slash D sharp", "m/D#", "m/D♯", "m/D♯")
+            case .minorSlashE:
+                return (" minor slash E", "m/E", "m/E", "m/E")
+            case .minorSlashF:
+                return (" minor slash F", "m/F", "m/F", "m/F")
+            case .minorSlashFSharp:
+                return (" minor slash F sharp", "m/F#", "m/F♯", "m/F♯")
+            case .minorSlashG:
+                return (" minor slash G", "m/G", "m/G", "m/G")
+            case .minorSlashGSharp:
+                return (" minor slash G sharp", "m/G#", "m/G♯", "m/G♯")
+            }
+        }
     }
 
     public static var guitar = Chords.readData(for: "GuitarChords")
