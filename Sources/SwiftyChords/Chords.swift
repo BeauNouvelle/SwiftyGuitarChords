131c3b670c2834a827cdc6a2bf1be1f685e4999a
Beau Nouvelle <beau@getswifty.dev>
2022-10-05 20:08:52 +1100

    added display text string to suffixes with correct capitalization

--- Sources/SwiftyChords/Chords.swift
+++ Sources/SwiftyChords/Chords.swift
@@ -81,6 +81,138 @@ public struct Chords {
         case minorSlashFSharp = "m/F#"
         case minorSlashG = "m/G"
         case minorSlashGSharp = "m/G#"
+        
+        /// A suitible string for displaying to users.
+        /// Same as the `rawValue` but with correct capitalization.
+        /// e.g. "mMaj7" rather than "mmaj7".
+        var display: String {
+            switch self {
+            case .major:
+                return "Major"
+            case .minor:
+                return "minor"
+            case .dim:
+                return "dim"
+            case .dimSeven:
+                return "dim7"
+            case .susTwo:
+                return "sus2"
+            case .susFour:
+                return "sus4"
+            case .sevenSusFour:
+                return "7sus4"
+            case .altered:
+                return "alt"
+            case .aug:
+                return "aug"
+            case .six:
+                return "6"
+            case .sixNine:
+                return "6/9"
+            case .seven:
+                return "7"
+            case .sevenFlatFive:
+                return "7b5"
+            case .augSeven:
+                return "aug7"
+            case .nine:
+                return "9"
+            case .nineFlatFive:
+                return "9b5"
+            case .augNine:
+                return "aug9"
+            case .sevenFlatNine:
+                return "7b9"
+            case .sevenSharpNince:
+                return "7#9"
+            case .eleven:
+                return "11"
+            case .nineSharpEleven:
+                return "9#11"
+            case .thirteen:
+                return "13"
+            case .majorSeven:
+                return "Maj7"
+            case .majorSevenFlatFive:
+                return "Maj7b5"
+            case .majorSevenSharpFive:
+                return "Maj7#5"
+            case .majorNine:
+                return "Maj9"
+            case .majorEleven:
+                return "Maj11"
+            case .majorThirteen:
+                return "Maj13"
+            case .minorSix:
+                return "m6"
+            case .minorSixNine:
+                return "m6/9"
+            case .minorSeven:
+                return "m7"
+            case .minorSevenFlatFive:
+                return "m7b5"
+            case .minorNine:
+                return "m9"
+            case .minorEleven:
+                return "m11"
+            case .minorMajorSeven:
+                return "mMaj7"
+            case .minorMajorSeventFlatFive:
+                return "mMaj7b5"
+            case .minorMajorNine:
+                return "mMaj9"
+            case .minorMajorEleven:
+                return "mMaj11"
+            case .addNine:
+                return "add9"
+            case .minorAddNine:
+                return "madd9"
+            case .slashE:
+                return "/E"
+            case .slashF:
+                return "/F"
+            case .slashFSharp:
+                return "/F#"
+            case .slashG:
+                return "/G"
+            case .slashGSharp:
+                return "/G#"
+            case .slashA:
+                return "/A"
+            case .slashBFlat:
+                return "/Bb"
+            case .slashB:
+                return "/B"
+            case .slashC:
+                return "/C"
+            case .slashCSharp:
+                return "/C#"
+            case .minorSlashB:
+                return "m/B"
+            case .minorSlashC:
+                return "m/C"
+            case .minorSlashCSharp:
+                return "m/C#"
+            case .slashD:
+                return "/D"
+            case .minorSlashD:
+                return "m/D"
+            case .slashDSharp:
+                return "/D#"
+            case .minorSlashDSharp:
+                return "m/D#"
+            case .minorSlashE:
+                return "m/E"
+            case .minorSlashF:
+                return "m/F"
+            case .minorSlashFSharp:
+                return "m/F#"
+            case .minorSlashG:
+                return "m/G"
+            case .minorSlashGSharp:
+                return "m/G#"
+            }
+        }
     }
 
     public static var guitar = Chords.readData(for: "GuitarChords")
